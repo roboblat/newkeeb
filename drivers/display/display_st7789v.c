2fc1a4b962fcc265b23c8f05f1736cb04b103695
roboblat <roboblat@gmail.com>
2026-08-02 02:35:08 -0400

    Set ROTATED_90 offset to x_offset=0, y_offset=20 for ST7789 landscape display

--- drivers/display/display_st7789v.c
+++ drivers/display/display_st7789v.c
@@ -318,7 +318,7 @@ static int st7789v_set_orientation(const struct device *dev,
 	case DISPLAY_ORIENTATION_ROTATED_90:
 		tx_data |= (ST7789V_MADCTL_MY_BOTTOM_TO_TOP | ST7789V_MADCTL_MV_REVERSE_MODE);
 		x_offset = 0;
-		y_offset = 0;
+		y_offset = data->y_offset;
 		break;
 
 	case DISPLAY_ORIENTATION_ROTATED_180:
